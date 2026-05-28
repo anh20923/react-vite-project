b23a43ac3167a2cf4d55ed7229c6f4bb8349c4ad
Anh Tran <anh.vainionpaa@gmail.com>
2026-05-28 06:53:17 +0300

    video31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,24 @@
 import "./style.css"
 
 const MyComponent = () => {
+    //const myVariable = "Hello Anh -"; //string
+    //const myVariable = 25; // number
+
+    // const myVariable = true; // boolean - html se khong hien len vi day la toan tu
+    //const myVariable = undefined; //  html se khong hien len vi day la toan tu
+    // const myVariable = null; //html se khong hien len vi day la toan tu
+
+    //object
+    //const myVariable = [1,2,3]
+    const myVariable = {
+        name: "Anh",
+        age: 25
+    }
+
     return (
         <>
-            <div> This is my component</div>
+            <div> {JSON.stringify(myVariable)} This is my component</div>
+            <div>{console.log("Hi hihiihi")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
