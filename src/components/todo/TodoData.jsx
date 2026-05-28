2c4f6570e995a2007f0de568bb5e0dafae2b74aa
Anh Tran <anh.vainionpaa@gmail.com>
2026-05-28 22:25:16 +0300

    video34: props, destructuring data

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,25 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props is an object{}
+    // {
+    //     name: "Anh",
+    //     age: 25,
+    //     data: {}
+    // }
+
+    //cach1: 
+    const { name, age, data } = props;
+
+    //cach2
+    //const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+
+    //cach3 khong nen lam
+
+    // console.log(">>> check props: ", props)
     return (
         <div className='todo-data'>
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
