aab02f23ad298604fb5ad59ba44ad132f7a6a80a
Anh Tran <anh.vainionpaa@gmail.com>
2026-05-31 20:26:36 +0300

    video38

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,19 @@ const App = () => {
     country: "vietnam"
   }
 
+  //create random id
+  const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+
   const addNewTodo = (name) => {
-    alert(`call me ${name}`)
+    const newTodo = {
+      id: randomIntFromInterval(1, 10000),
+      name: name
+    }
+
+    setTodoList([...todoList, newTodo])
   }
 
 
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -11,7 +11,7 @@ const TodoNew = (props) => {
     //addNewTodo("yumi")
 
     const handleClick = () => {
-        console.log(">>>> check valueInput: ", valueInput)
+        addNewTodo(valueInput)
     }
 
     const handleOnChange = (name) => {
