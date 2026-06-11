dcce93d017b3f4beaef3bdbcbd316b7544c565de
Anh Tran <anh.vainionpaa@gmail.com>
2026-06-12 01:21:20 +0300

    video51

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,14 @@
-import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData'
-import TodoNew from './components/todo/TodoNew'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
+
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
 import { Outlet } from 'react-router-dom'
 
+
+
 const App = () => {
-
-  const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Youtube" }
-  ])
-
-  const name = "Anh K";
-  const age = 25;
-  const data = {
-    address: "hochiminh",
-    country: "vietnam"
-  }
-
-  //create random id
-  const randomIntFromInterval = (min, max) => { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id: randomIntFromInterval(1, 10000),
-      name: name
-    }
-
-    setTodoList([...todoList, newTodo])
-  }
-
-
-  //{key:value}
   return (
     <>
       <Header />
-      <div className="todo-container">
-        <div className='todo-title'>Todo List</div>
-        <TodoNew
-          addNewTodo={addNewTodo} />
-        <TodoData
-          name={name}
-          age={age}
-          data={data}
-          todoList={todoList}
-
-        />
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo' />
-        </div>
-      </div>
       <Outlet />
       <Footer />
     </>
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,54 @@
+import './todo.css'
+import TodoData from './TodoData'
+import TodoNew from './TodoNew'
+import reactLogo from '../../assets/react.svg'
+import { useState } from 'react'
+
+const TodoApp = () => {
+    const [todoList, setTodoList] = useState([
+        { id: 1, name: "Learning React" },
+        { id: 2, name: "Watching Youtube" }
+    ])
+
+    const name = "Anh K";
+    const age = 25;
+    const data = {
+        address: "hochiminh",
+        country: "vietnam"
+    }
+
+    //create random id
+    const randomIntFromInterval = (min, max) => { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+
+    const addNewTodo = (name) => {
+        const newTodo = {
+            id: randomIntFromInterval(1, 10000),
+            name: name
+        }
+
+        setTodoList([...todoList, newTodo])
+    }
+
+    return (
+        <div className="todo-container">
+            <div className='todo-title'>Todo List</div>
+            <TodoNew
+                addNewTodo={addNewTodo} />
+            <TodoData
+                name={name}
+                age={age}
+                data={data}
+                todoList={todoList}
+
+            />
+            <div className='todo-image'>
+                <img src={reactLogo} className='logo' />
+            </div>
+        </div>
+    )
+}
+
+export default TodoApp;
