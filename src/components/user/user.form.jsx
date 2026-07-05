ab66a7d70995f68ad135f01c7977807710df7a19
Anh Tran <anh.vainionpaa@gmail.com>
2026-07-05 22:09:01 +0300

    video69 - create new user, import axios

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,7 @@
 import { Input } from 'antd';
 import { Button } from 'antd';
 import { useState } from 'react';
+import axios from "axios"
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("")
@@ -11,6 +12,14 @@ const UserForm = () => {
     // console.log(">>>> check form: ", fullName, email, password, phone)
 
     const handleClickBtn = () => {
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data)
         console.log(">>>>> check state: ", { fullName, email, password, phone })
     }
 
