2193afc0518da36a6d477d760fd38abeb0fee389
Anh Tran <anh.vainionpaa@gmail.com>
2026-07-05 23:52:16 +0300

    video70 - Config Axios Interceptor

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,7 +1,8 @@
-import { Input } from 'antd';
+import { Input, message, notification } from 'antd';
 import { Button } from 'antd';
 import { useState } from 'react';
-import axios from "axios"
+import { createUserAPI } from '../../services/api.service';
+
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("")
@@ -11,16 +12,16 @@ const UserForm = () => {
 
     // console.log(">>>> check form: ", fullName, email, password, phone)
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
-        }
-        axios.post(URL_BACKEND, data)
-        console.log(">>>>> check state: ", { fullName, email, password, phone })
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "create use",
+                description: "Create user successful"
+            })
+        } // phai check ca 2 TH vi co the res.data = null
+
+        console.log(">>>>> check state: ", res.data)
     }
 
     return (
